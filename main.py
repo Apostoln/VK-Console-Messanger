e78cfece491ea309b6895a1311c32b3f29687089
Mykyta Bondarenko <nikbond97@gmail.com>
2017-02-01 18:05:31 +0200

    Initial commit

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-
+import vk
+import sys
 
 def main():
-    pass
+    TOKEN = sys.argv[1]
+    session = vk.Session(access_token=TOKEN)
+    api = vk.API(session, v='5.62', lang='ru', timeout=10)
 
 if __name__ == '__main__':
     main()
